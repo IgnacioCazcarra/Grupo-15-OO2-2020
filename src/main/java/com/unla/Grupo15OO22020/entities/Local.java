45ea0aaf45f895d0812844ff7b3a48002f7363fc
unknown <borsafranco@gmail.com>
2020-05-03 21:20:21 -0300

    Entidades Local y Persona corregidas (caracteres basura)

--- src/main/java/com/unla/Grupo15OO22020/entities/Local.java
+++ src/main/java/com/unla/Grupo15OO22020/entities/Local.java
@@ -1,29 +1,8 @@
 package com.unla.Grupo15OO22020.entities;
 
 
-<<<<<<< HEAD
+
 import java.time.LocalDateTime;
-
-import java.util.HashSet;
-import java.util.Set;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToMany;
-import javax.persistence.Table;
-
-import javax.persistence.FetchType;
-
-import javax.persistence.OneToMany;
-
-import org.hibernate.annotations.CreationTimestamp;
-@Entity
-@Table(name = "local")
-=======
 import java.time.LocalTime;
 import java.util.HashSet;
 import java.util.Set;
@@ -46,22 +25,19 @@ import org.hibernate.annotations.UpdateTimestamp;
 
 @Entity
 @Table(name="local")
->>>>>>> 97632ef2fb85451952e9a799f546dd0c5f3bd65f
 public class Local {
-
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long idLocal;
-<<<<<<< HEAD
 
 	@Column(name = "direccion")
 	private String direccion;
 	
 	@Column(name = "latitud")
-	private String latitud;
+	private double latitud;
 	
 	@Column(name = "longitud")
-	private String longitud;
+	private double longitud;
 
 	@Column(name = "telefono")
 	private String telefono;
@@ -88,66 +64,22 @@ public class Local {
 	@CreationTimestamp
 	private LocalDateTime updateAt;
 	
-	
-	public Local() {}
-	
-	public Local(long idLocal, String direccion, String latitud, String longitud, String telefono) {
-=======
-	
-	@Column(name="direccion")
-	private String direccion;
-	
-	@Column(name="latitud")
-	private double latitud;
-	
-	@Column(name="longitud")
-	private double longitud;
-	
-	@Column(name="telefono")
-	private String telefono;
-	
-	
 	@OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "stock_id", referencedColumnName = "idStock")
     private Stock stock;
 	
+	public Local() {}
 	
-	@ManyToMany(mappedBy = "listaLocales")
-	private Set<Cliente> listaClientes = new HashSet<Cliente>();
-	
-	@OneToMany(fetch = FetchType.LAZY, mappedBy = "local")
-	private Set<Empleado> listaEmpleados = new HashSet<Empleado>();
-//	@OneToMany(fetch = FetchType.LAZY, mappedBy="tablaLocal")
-//	private Set<Factura> listaFacturas = new HashSet<Factura>();
-	
-//	@OneToMany(fetch = FetchType.LAZY, mappedBy="tablaLocal")
-//	private Set<SolicitudStock> listaSolicitudesStock = new HashSet<SolicitudStock>();
-	
-	
-	
-	@Column(name="createdat")
-	@CreationTimestamp
-	private LocalTime createdAt;
-	
-	@Column(name="updatedat")
-	@UpdateTimestamp
-	private LocalTime updatedAt;
-
-	public Local() {
-		super();
-	}
 
 	public Local(long idLocal, String direccion, double latitud, double longitud, String telefono) {
->>>>>>> 97632ef2fb85451952e9a799f546dd0c5f3bd65f
 		super();
 		this.idLocal = idLocal;
 		this.direccion = direccion;
 		this.latitud = latitud;
 		this.longitud = longitud;
 		this.telefono = telefono;
-<<<<<<< HEAD
-		
-=======
+
+
 	}
 
 	
@@ -158,7 +90,6 @@ public class Local {
 
 	public void setStock(Stock stock) {
 		this.stock = stock;
->>>>>>> 97632ef2fb85451952e9a799f546dd0c5f3bd65f
 	}
 
 	public long getIdLocal() {
@@ -178,21 +109,6 @@ public class Local {
 		this.direccion = direccion;
 	}
 
-<<<<<<< HEAD
-	public String getLatitud() {
-		return latitud;
-	}
-
-	public void setLatitud(String latitud) {
-		this.latitud = latitud;
-	}
-
-	public String getLongitud() {
-		return longitud;
-	}
-
-	public void setLongitud(String longitud) {
-=======
 	public double getLatitud() {
 		return latitud;
 	}
@@ -206,7 +122,6 @@ public class Local {
 	}
 
 	public void setLongitud(double longitud) {
->>>>>>> 97632ef2fb85451952e9a799f546dd0c5f3bd65f
 		this.longitud = longitud;
 	}
 
@@ -225,14 +140,18 @@ public class Local {
 	public void setListaClientes(Set<Cliente> listaClientes) {
 		this.listaClientes = listaClientes;
 	}
-<<<<<<< HEAD
+
+
+	public Set<Empleado> getListaEmpleados() {
+		return listaEmpleados;
+	}
+
+
+	public void setListaEmpleados(Set<Empleado> listaEmpleados) {
+		this.listaEmpleados = listaEmpleados;
+	}
+
 
 }
-=======
-	
-	
-	
-	
-}
 
->>>>>>> 97632ef2fb85451952e9a799f546dd0c5f3bd65f
+	
