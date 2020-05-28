096801c999b50e1eda25411bcaf1cec6f56d60b6
Ignacio Cazcarra <nacho.eze.cazcarra@gmail.com>
2020-05-28 14:44:15 -0300

    Subo avances del caso 11. Pedido pasa al post como null.

--- src/main/java/com/unla/Grupo15OO22020/models/PedidoModel.java
+++ src/main/java/com/unla/Grupo15OO22020/models/PedidoModel.java
@@ -117,56 +117,5 @@ public class PedidoModel {
 		this.fecha = fecha;
 	}
 
-	
-	
-	/*
-	 * public PedidoModel(ProductoModel producto, int cantidad, LocalModel local,
-	 * ClienteModel cliente, EmpleadoModel vendedorOriginal, EmpleadoModel
-	 * vendedorAuxiliar, boolean aceptado) { super();
-	 * 
-	 * this.producto = producto; this.cantidad = cantidad; this.local = local;
-	 * this.cliente = cliente; this.vendedorOriginal = vendedorOriginal;
-	 * this.vendedorAuxiliar = vendedorAuxiliar; this.aceptado = aceptado;
-	 * this.subtotal = CalcularSubtotal(); }
-	 * 
-	 * public ProductoModel getProducto() { return producto; }
-	 * 
-	 * public void setProducto(ProductoModel producto) { this.producto = producto; }
-	 * 
-	 * public int getCantidad() { return cantidad; }
-	 * 
-	 * public void setCantidad(int cantidad) { this.cantidad = cantidad; }
-	 * 
-	 * public boolean isAceptado() { return aceptado; }
-	 * 
-	 * public void setAceptado(boolean aceptado) { this.aceptado = aceptado; }
-	 * 
-	 * public LocalModel getLocal() { return local; }
-	 * 
-	 * public void setLocal(LocalModel local) { this.local = local; }
-	 * 
-	 * public ClienteModel getCliente() { return cliente; }
-	 * 
-	 * public void setCliente(ClienteModel cliente) { this.cliente = cliente; }
-	 * 
-	 * public EmpleadoModel getVendedorOriginal() { return vendedorOriginal; }
-	 * 
-	 * public void setVendedorOriginal(EmpleadoModel vendedorOriginal) {
-	 * this.vendedorOriginal = vendedorOriginal; }
-	 * 
-	 * public EmpleadoModel getVendedorAuxiliar() { return vendedorAuxiliar; }
-	 * 
-	 * public void setVendedorAuxiliar(EmpleadoModel vendedorAuxiliar) {
-	 * this.vendedorAuxiliar = vendedorAuxiliar; }
-	 * 
-	 * public long getIdPedido() { return idPedido; }
-	 * 
-	 * protected void setIdPedido(long idPedido) { this.idPedido = idPedido; }
-	 * 
-	 * public float getTotal() { return subtotal; }
-	 * 
-	 * public void setTotal(float subtotal) { this.subtotal = subtotal; }
-	 * 
-	 */
 
 }
