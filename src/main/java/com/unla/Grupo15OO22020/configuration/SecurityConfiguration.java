20a55f199c0c02ee063ba61a65c17789dfca285b
Ignacio Cazcarra <nacho.eze.cazcarra@gmail.com>
2020-06-10 20:02:36 -0300

    fix pequeñisimo para los accesos segun el rol (lo termina seba)

--- src/main/java/com/unla/Grupo15OO22020/configuration/SecurityConfiguration.java
+++ src/main/java/com/unla/Grupo15OO22020/configuration/SecurityConfiguration.java
@@ -31,10 +31,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 				.antMatchers("/css/*", "/imgs/*", "/js/*", "/vendor/bootstrap/css/*", "/vendor/jquery/*", "/vendor/bootstrap/js/*").permitAll()
-				.antMatchers("/empleados","/empleados/*").hasRole("ADMIN")  // GERENTE
-				.antMatchers("/locales","/locales/*").hasRole("ADMIN")	    // GERENTE	
-
-				.antMatchers("/pedidos","/pedidos/*").authenticated()		// TIENE QUE ESTAR LOGUEADO PARA INGRESAR
+				.antMatchers("/locales/new","/locales/create","/locales/update","/empleados/new","/empleados/create","/empleados/update").hasRole("ADMIN")// GERENTE	
+				.antMatchers("/pedidos","/pedidos/*","pedidos/solicitudstock").authenticated()		// TIENE QUE ESTAR LOGUEADO PARA INGRESAR
 				.antMatchers("/productos","/productos/*").authenticated()   // TIENE QUE ESTAR LOGUEADO PARA INGRESAR
 				.antMatchers("/lotes","/lotes/*").authenticated()           // TIENE QUE ESTAR LOGUEADO PARA INGRESAR
 				.antMatchers("/stocks","/stocks/*").authenticated()         // TIENE QUE ESTAR LOGUEADO PARA INGRESAR 
